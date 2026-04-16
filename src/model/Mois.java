d60152c246658d65f14ce2d32e1052b4c1d906dd
me56de <mederic.mognat@utoulouse.fr>
2026-04-16 19:51:48 +0200

    TPC

---
+++ src/model/Mois.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Mois {
+	private String nom;
+	private boolean[] jours;
+	
+	
+	public boolean estLibre(String jour) {
+		
+	}
+	
+	public void reserver(String reserver) {
+		int taille = jours.length;
+		jours[taille] = ;
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,4 @@
+module ILU2_POO_TP_Commun {
+	requires java.desktop;
+	requires org.junit.jupiter.api;
+}
